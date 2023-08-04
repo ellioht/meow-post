e4c631545052bc934a9166d09eafc19a3f827cf8
Elliot <130664947+ellioht@users.noreply.github.com>
2023-08-05 00:09:27 +0100

    create files

---
+++ app/layout.jsx
@@ -0,0 +1,25 @@
+import "@/styles/globals.css";
+import Nav from "@/components/Nav";
+import Provider from "@/components/Provider";
+
+export const metadata = {
+  title: "Meow Post",
+  description: "Create and share posts from yourself and other cats around the world!",
+};
+
+const Layout = ({ children }) => {
+  return (
+    <html lang="en">
+      <body>
+        <Provider>
+          <main className="app">
+            <Nav />
+            {children}
+          </main>
+        </Provider>
+      </body>
+    </html>
+  );
+};
+
+export default Layout;
